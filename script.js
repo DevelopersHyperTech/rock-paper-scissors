1bd33aa6831e1eb2c368cffb0bd54824edbcb736
DevelopersHyperTech <119522654+DevelopersHyperTech@users.noreply.github.com>
2023-02-06 18:55:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,38 @@ const resultDisplay = document.getElementById('result');
 const aiScore = document.getElementById('ai-score');
 const userScore = document.getElementById('user-score')
 const ai = document.querySelector('.game-container .ai')
+const endGame = document.getElementById('end-game-container')
+const HeadText = document.getElementById('head-text')
+const paragraghText = document.getElementById('paragragh-text')
 let userscore = 0
 let aiscore = 0
 let aiChoice
 let userChoice
 let result
-
+    // Increase Abdulrahman's Score
+function incremntAiScore() {
+    aiscore++
+    aiScore.innerHTML = aiscore
+}
+// Increase user score
+function incremntUserScore() {
+    userscore++
+    userScore.innerHTML = userscore
+}
 var current_mode = screen.orientation;
-
+// Change view to landscape
 // type
 console.log(current_mode)
 
 // angle
 console.log(current_mode.angle)
-
-
 if (window.onload)
     document.querySelector("body").requestFullscreen();
 else if (window.onload)
     document.querySelector("body").webkitRequestFullScreen();
-
-
-
 screen.orientation.lock("landscape")
+
+// Event Listener on click
 userButton.forEach(button => button.addEventListener('click', (e) => {
     userChoice = e.target.id
     generateRandomChoice();
@@ -37,18 +46,11 @@ userButton.forEach(button => button.addEventListener('click', (e) => {
     getResult();
 }));
 
-function incremntAiScore() {
-    aiscore++
-    aiScore.innerHTML = aiscore
-    console.log(aiscore);
-}
-
-function incremntUserScore() {
-    userscore++
-    userScore.innerHTML = userscore
-}
-
+// Generate random choice
 function generateRandomChoice() {
+    setTimeout(() => {
+        aiButton.innerHTML = '⏳'
+    }, 1500);
     const randomNumber = Math.floor(Math.random() * 3) + 1
     if (randomNumber === 1) {
         aiChoice = 'rock'
@@ -62,9 +64,7 @@ function generateRandomChoice() {
         aiChoice = 'scissors'
         aiButton.innerText = '✌🏻'
     }
-    setTimeout(() => {
-        aiButton.innerHTML = '⏳'
-    }, 1500);
+
 }
 
 function getResult() {
@@ -76,7 +76,7 @@ function getResult() {
         incremntUserScore();
     }
     if (aiChoice === 'rock' && userChoice === "scissors") {
-        result = 'you lost!'
+        result = 'you lose!'
         incremntAiScore();
     }
     if (aiChoice === 'paper' && userChoice === "scissors") {
@@ -94,6 +94,13 @@ function getResult() {
     if (aiChoice === 'scissors' && userChoice === "paper") {
         result = 'you lose!'
         incremntAiScore();
+    }
+    if (userscore === 10) {
+        endGame.style.display = 'flex'
+    } else if (aiscore === 10) {
+        endGame.style.display = 'flex'
+        HeadText.innerText = 'Oh Uh! You Lose'
+        paragraghText.innerHTML = 'Game over'
     }
     resultDisplay.innerHTML = result
 }
